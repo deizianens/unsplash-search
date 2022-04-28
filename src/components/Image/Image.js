148e3eb1cd6bad69bded0eaa87003cd246d56c1a
Deiziane Silva <deiziane_ns@hotmail.com>
2022-04-28 09:49:16 -0300

    creates image component

---
+++ src/components/Image/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Image = () => {
+  return (
+    <img
+      src="https://images.unsplash.com/photo-1650969206434-622016de7428?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+      alt=""
+    />
+  );
+};
+
+export default Image;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export { default as Search } from "./Search";
+export { default as Search } from "./Search/Search";
 export { default as Dropdown } from "./Dropdown/Dropdown";
+export { default as Image } from "./Image/Image";
